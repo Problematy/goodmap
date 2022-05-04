1542db023fa07f9e2ba4f2b0b39ef07b43247df8
Krzysztof <krzysztof.kolodzinski@lifeflow.eu>
2022-05-05 01:48:30 +0200

    first part of async

--- goodmap/static/map.js
+++ goodmap/static/map.js
@@ -2,33 +2,53 @@ $.ajaxSetup({
     async: false
 });
 
-var data  = $.getJSON("/data").responseJSON
-var categories = $.getJSON("/api/categories").responseJSON
-var map   = L.map('map').setView([51.1,17.05], 13);
-var layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-})
+$( document ).ready( function() {
+    var markers = L.markerClusterGroup();
+    var data  = $.getJSON("/data").then( function(response) {
+      response.map(x => L.marker(x.position).addTo(markers).bindPopup(x.name));
+    });
+    var categories = $.getJSON("/api/categories").responseJSON;
+    var checkbox_data = categories.map(x => [x, $.getJSON("/api/category/"+x).responseJSON]);
 
-var markers = L.markerClusterGroup();
+    var mainMap   = L.map('map').setView([51.1,17.05], 13);
+    var layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    });
 
-data.map(x => L.marker(x.position).addTo(markers).bindPopup(x.name));
-layer.addTo(map);
-map.addLayer(markers);
+    layer.addTo(mainMap);
+    mainMap.addLayer(markers);
 
-var command = L.control({position: 'topright'});
-command.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'command');
-    var form = document.createElement('form');
-    var checkbox_data = categories.map(x => [x, $.getJSON("/api/category/"+x).responseJSON])
-    checkbox_data.forEach( x =>
-        x[1].forEach(y => form.appendChild(createCheckboxWithType(x[0], y)))
-    )
+    var command = L.control({position: 'topright'});
+    command.onAdd = function (mainMap) {
+        var div = L.DomUtil.create('div', 'command');
+        var form = document.createElement('form');
 
-    div.appendChild(form);
-    return div;
-};
-command.addTo(map);
+        checkbox_data.forEach( x =>
+            x[1].forEach(y => form.appendChild(createCheckboxWithType(x[0], y)))
+        )
+
+        div.appendChild(form);
+        return div;
+    };
+    command.addTo(mainMap);
+
+    $(".filter").on('click', function(){
+        mainMap.removeLayer(markers);
+        markers = L.markerClusterGroup();
+
+        var filterros = categories.map(x => $.getJSON("/api/category/"+x).responseJSON);
+        var all_checkboxes = categories.map(x => getSpecificCheckboxes(x));
+        var filteros = all_checkboxes.filter(n => n).join('&');
+
+        var url = ["/data", filteros].filter(n => n).join('?');
+        var new_data = $.getJSON(url).responseJSON;
+        new_data.map(x => L.marker(x.position).addTo(markers).bindPopup(x.name));
+        mainMap.addLayer(markers);
+    });
+
+});
+
 
 function createCheckboxWithType(filter_type, entry) {
     var main = document.createElement("div");
@@ -36,7 +56,7 @@ function createCheckboxWithType(filter_type, entry) {
     label.htmlFor = entry;
     label.innerHTML = entry;
     var checkbox = document.createElement("input");
-    checkbox.className = "filter "+filter_type
+    checkbox.className = "filter "+filter_type;
     checkbox.type = "checkbox";
     checkbox.name = "name";
     checkbox.value = entry;
@@ -47,24 +67,10 @@ function createCheckboxWithType(filter_type, entry) {
     return main;
 }
 
-$(".filter").on('click', function(){
-    map.removeLayer(markers);
-    markers = L.markerClusterGroup();
-
-    var filterros = categories.map(x => $.getJSON("/api/category/"+x).responseJSON)
-    var all_checkboxes = categories.map(x => getSpecificCheckboxes(x))
-    var filteros = all_checkboxes.filter(n => n).join('&');
-
-    var url = ["/data", filteros].filter(n => n).join('?');
-    var new_data = $.getJSON(url).responseJSON;
-    new_data.map(x => L.marker(x.position).addTo(markers).bindPopup(x.name));
-    map.addLayer(markers);
-});
-
 function getSpecificCheckboxes(filter_type){
-    var selector = ".filter."+filter_type+":checked"
+    var selector = ".filter."+filter_type+":checked";
     var select = $(selector);
     var checked_boxes_types = $(".filter."+filter_type+":checked").toArray();
     var types = checked_boxes_types.map(x => filter_type + '=' + x.value).join('&');
-    return types
+    return types;
 }
