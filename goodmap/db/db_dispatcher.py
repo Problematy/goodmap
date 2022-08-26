92f7b80a980fa95eca3f4f087477ecf8bff93e0a
Trotter-Wiz-Kiwanis <trotter.wiz.kiwanis+github@proton.me>
2022-08-26 21:21:11 +0200

    Add fallback data for local testing
    Add fallback data for local testing

--- goodmap/db/db_dispatcher.py
+++ goodmap/db/db_dispatcher.py
@@ -3,8 +3,11 @@ from .google_json_db import GoogleJsonDb
 
 
 def get_db(db_config):
-    config_loaders = {
-        "json_file": lambda x: LocalJsonDb(x),
-        "google_hosted_json_file": lambda x: GoogleJsonDb(x)
+    db_loaders = {
+        "json_file": lambda local: LocalJsonDb(local),
+        "google_hosted_json_file": lambda hosted: GoogleJsonDb(hosted),
     }
-    return config_loaders[db_config["type"]](db_config)
+
+    db_type = db_config["type"]
+    selected_db_loader = db_loaders[db_type]
+    return selected_db_loader(db_config)
