f16c4eb8e41cd206d66e0c2553ad24936a9ab77c
Trotter-Wiz-Kiwanis <trotter.wiz.kiwanis+github@proton.me>
2022-08-27 14:54:38 +0200

    Handle missing 'type_of_place' key

--- goodmap/formatter.py
+++ goodmap/formatter.py
@@ -9,10 +9,21 @@ def safe_gettext(lista):
 
 
 def prepare_pin(place, visible_fields):
+    type_of_place = get_type_of_place(place)
     pin_data = {
         "title": place["name"],
-        "subtitle": lazy_gettext(place["type_of_place"]),
+        "subtitle": lazy_gettext(type_of_place),
         "position": place["position"],
-        "data": {gettext(field): safe_gettext(place[field]) for field in visible_fields}
+        "data": {
+            gettext(field): safe_gettext(place[field]) for field in visible_fields
+        },
     }
     return pin_data
+
+
+def get_type_of_place(place):
+    # TODO: should we require 'type_of_place'? For now, this workaround
+    try:
+        return place["type_of_place"]
+    except KeyError:
+        return "type_of_place"
