ae01b0d79197b0ea896bf12b66974b7d3bb0e784
Krzysztof <krzysztof.kolodzinski@lifeflow.eu>
2022-09-02 14:09:39 +0200

    moved translations to project repository

--- translations/extractor.py
+++
@@ -1,9 +0,0 @@
-def extract_added_words(fileobj, keywords, comment_tags, options):
-    def yielder(data):
-        for value in data:
-            yield (None, None, [value], [])
-
-    encoding = "utf-8"
-    data = fileobj.read().decode(encoding).splitlines()
-
-    return yielder(data)
