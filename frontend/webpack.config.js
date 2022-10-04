843db508e6c06c5e7712a111c9ccf14c758c1f93
ravenwing <kolodzinski.krzys@gmail.com>
2022-10-05 01:57:32 +0200

    Fix building minified js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,15 +1,26 @@
 const webpack = require('webpack');
-module.exports = {
+const TerserPlugin = require('terser-webpack-plugin');
+
+
+module.exports = (env, argv) => {
+  const IS_PROD = argv.mode == 'production'
+
+ return {
     devtool: 'source-map',
-    entry:  './src/map.js',
+    entry: "./src/map.js",
     output: {
         path: __dirname + '/dist',
-        filename: 'map.js',
+        filename: IS_PROD ? 'map.min.js' : 'map.js',
     },
     resolve: {
         extensions: ['.js', '.jsx', '.css']
     },
-
+    optimization: {
+      minimize: IS_PROD,
+      minimizer: [
+        new TerserPlugin({ parallel: true })
+      ]
+    },
     module: {
         rules: [
             {
@@ -19,4 +30,5 @@ module.exports = {
             }
         ]
     }
+  }
 };
