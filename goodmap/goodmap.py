ff21f9926df65440d9824be4db13a1a0373d63ad
Krzysztof <krzysztof.kolodzinski@lifeflow.eu>
2022-10-27 02:24:50 +0200

    moved to platzky

--- goodmap/goodmap.py
+++ goodmap/goodmap.py
@@ -1,42 +1,42 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+from platzky import platzky
+
+from flask import Flask, render_template, request, redirect, session, url_for, Blueprint
 from flask_babel import Babel
 import yaml
 import os
+from os import path
 
-from .db import get_db
+from goodmap.db.extend import extend_app_db
 from .core_api import core_pages
 
 
 def create_app(config_path):
-    app = Flask(__name__)
-    absolute_config_path = os.path.join(os.getcwd(), config_path)
-    app.config.from_file(absolute_config_path, load=yaml.safe_load)
+    config = platzky.config.from_file(config_path)
+    config.add_translations_dir(path.join(path.dirname(__file__), "./translations"))
 
-    app.db = get_db(app.config["DB"])
+    app = platzky.create_app_from_config(config)
+    extend_app_db(app)
+
     app.register_blueprint(core_pages(app.db, app.config["LANGUAGES"]))
-    app.babel = Babel(app)
+    main = create_app_original(config)
+    app.register_blueprint(main)
 
-    if overwrites := app.config.get("ROUTE_OVERWRITES"):
+    return app
+
+
+def create_app_original(config):
+    url_prefix = "/"
+    goodmap = Blueprint('goodmap', __name__,
+                         url_prefix=url_prefix, template_folder='templates')
+
+    if overwrites := config.asdict().get("ROUTE_OVERWRITES"):
         for source, destination in overwrites.items():
-            @app.route(source)
+            @goodmap.route(source)
             def testing_map():
                 return redirect(destination)
 
-    @app.babel.localeselector
-    def get_locale():
-        return session.get('language', request.accept_languages.best_match(app.config["LANGUAGES"]))
-
-    @app.route('/language/<language>')
-    def set_language(language):
-        session['language'] = language
-        return redirect(url_for('index'))
-
-    @app.context_processor
-    def setup_context():
-        return dict(app_name=app.config['APP_NAME'])
-
-    @app.route("/")
+    @goodmap.route("/")
     def index():
         return render_template('map.html')
 
-    return app
+    return goodmap
