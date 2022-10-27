426ffbcdb1fb1dee4363794e5d70ee3d35ac53b1
ravenwing <kolodzinski.krzys@gmail.com>
2022-10-27 02:47:50 +0200

    Made goodmap use platzky engine
    * fix for languages
    
    * moved to platzky
    
    * moved domain in tests to configuration

--- e2e-tests/tests/e2e_tests/cypress.config.js
+++ e2e-tests/tests/e2e_tests/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://www.goodmap.localhost:5000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
