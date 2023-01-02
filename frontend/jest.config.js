0090a4f4137eddaa788ab378cc1b425d60f00949
Krzysztof <krzysztof.kolodzinski@lifeflow.eu>
2023-01-02 21:32:32 +0100

    fixed missing coverage of untested files

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,9 @@ const config = {
     verbose: true,
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    collectCoverageFrom: [
+	"src/components/**/*.jsx"
+    ]
 };
 
 module.exports = config;
