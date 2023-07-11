f8047514df5bec8f3f8cfbb0b1d4112a7ac3d55e
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2023-07-11 23:00:57 +0200

    fix config

--- tests/unit_tests/test_config.py
+++ tests/unit_tests/test_config.py
@@ -1,6 +1,5 @@
 from goodmap.config import Config
 
-
 def test_parse_template_config() -> None:
     """Test that the template config can be parsed."""
     Config.parse_yaml("config-template.yml")
