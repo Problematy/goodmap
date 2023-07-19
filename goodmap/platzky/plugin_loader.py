1dc1d6017f6ee621472172b270865551812fcd25
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2023-07-20 00:29:57 +0200

    changed some names

--- goodmap/platzky/plugin_loader.py
+++ goodmap/platzky/plugin_loader.py
@@ -40,7 +40,9 @@ def find_plugins(enabled_plugins):
     return plugins
 
 
-def plugify(app, plugins):
-    for plugin in find_plugins(plugins.keys()):
-        plugin.process(app, plugins[plugin.__name__])
+def plugify(app, plugins_configs):
+    plugins_names = plugins_configs.keys()
+    plugins = find_plugins(plugins_names)
+    for plugin in plugins:
+        plugin.process(app, plugins_configs[plugin.__name__])
     return app
