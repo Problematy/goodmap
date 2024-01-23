c539a4cb0784bc7da3aa136b009646a5e81849dd
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-01-23 23:20:08 +0100

    removed another pyright ignore

--- goodmap/platzky/blog/post_formatter.py
+++ goodmap/platzky/blog/post_formatter.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Any, Dict
 
 import humanize
 
@@ -11,7 +12,12 @@ def get_delta(now, date_in):
 
 def format_post(post):
     now = datetime.datetime.now()
+
+    def comment_sort_key(comment_: Dict[str, Any]) -> Any:
+        return comment_["date"]
+
     for comment in post["comments"]:
         comment.update({"time_delta": get_delta(now, comment["date"])})
-    post["comments"].sort(key=lambda x: x["date"], reverse=True)  # pyright: ignore
+
+    post["comments"].sort(key=comment_sort_key, reverse=True)
     return post
