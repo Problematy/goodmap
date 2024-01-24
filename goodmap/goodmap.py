b7ee07c7a488e72d400a2b9424966e24ed1f8b22
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-01-24 15:04:26 +0100

    fix lint

--- goodmap/goodmap.py
+++ goodmap/goodmap.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, render_template
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 
 from goodmap.platzky import platzky
