3dfc547d9438e63dca91afac49c2a661d1659c8a
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-07-26 15:32:41 +0200

    Separate checking obligatory fields and categories

--- goodmap/data_validator.py
+++ goodmap/data_validator.py
@@ -5,25 +5,33 @@ def check_json_validaty(json_file):
     json_data = json.load(json_file)
     return json_data
 
-def check_obligatory_fields(datapoints, categories):
+def check_obligatory_fields(datapoints, obligatory_fields):
+    for p in datapoints:
+        for field in obligatory_fields:
+            if field not in p.keys():
+                raise Exception(f'ERROR datapoint: \n {p} \nis missing obligatory field: "{field}"')
+
+def check_categories_values(datapoints, categories):
     for p in datapoints:
         for category in categories:
             if category not in p.keys():
-                raise Exception(f"ERROR datapoint: \n {p} \nis missing field: {category}")
+                raise Exception(f'ERROR datapoint: \n {p} \nis missing obligatory category: "{category}"')
             if type(p[category]) is list: 
                 for attribute_value in p[category]:
                     if attribute_value not in categories[category]:
-                        raise Exception(f"ERROR, datapoint: \n {p} \nhas an invalid value for category: {category}")
+                        raise Exception(f'ERROR, datapoint: \n {p} \nhas an invalid value for category: "{category}"')
             else:
                 if p[category] not in categories[category]:
-                    raise Exception(f"ERROR, datapoint: \n {p} \nhas an invalid value for category: {category}")
+                    raise Exception(f'ERROR, datapoint: \n {p} \nhas an invalid value for category: "{category}"')
+
 
 def check_for_null_values(datapoints):
     for p in datapoints:
     ### check for null values in non-obligatory fields    
         for attribute in p.keys():
             if p[attribute] is None:
-                raise Exception(f"ERROR, datapoint: \n {p} \nhas a null value for field: {attribute}")
+                raise Exception(f'ERROR, datapoint: \n {p} \nhas a null value for field: "{attribute}"')
+
 
 def validate_from_json(json_file_path):
     with open(json_file_path) as json_file:
@@ -34,17 +42,22 @@ def validate_from_json(json_file_path):
             print(e)
             exit(-1)
 
+    datapoints = json_data["map"]["data"]
     categories = json_data["map"]["categories"]
-    points = json_data["map"]["data"]
+    obligatory_fields = json_data["map"]["obligatory_fields"]
 
     try:
-        check_obligatory_fields(points, categories)
+        check_obligatory_fields(datapoints, obligatory_fields)
     except Exception as e:
         print(e)
         exit(-1)
-
     try:
-        check_for_null_values(points)
+        check_categories_values(datapoints, categories)
+    except Exception as e:
+        print(e)
+        exit(-1)
+    try:
+        check_for_null_values(datapoints)
     except Exception as e:
         print(e)
         exit(-1)
