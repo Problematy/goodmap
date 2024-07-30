28d9d2dc8e05b2083b0398aac78403ffc9fac234
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-07-30 16:24:52 +0200

    Style: give better names to variables

--- goodmap/data_validator.py
+++ goodmap/data_validator.py
@@ -3,16 +3,16 @@ from sys import argv, stderr
 
 
 def get_missing_obligatory_fields(datapoints, obligatory_fields):
-    message = []
+    invalid_points = []
     for p in datapoints:
         for field in obligatory_fields:
             if field not in p.keys():
-                message.append(("missing obligatory field", p, field))
-    return message
+                invalid_points.append(("missing obligatory field", p, field))
+    return invalid_points
 
 
 def get_categories_with_invalid_values(datapoints, categories):
-    message = []
+    invalid_points = []
     for p in datapoints:
         for category in categories & p.keys():
             category_value = p[category]
@@ -20,20 +20,20 @@ def get_categories_with_invalid_values(datapoints, categories):
             if type(category_value) is list:
                 for attribute_value in category_value:
                     if attribute_value not in valid_values_set:
-                        message.append(("invalid category value", p, category))
+                        invalid_points.append(("invalid category value", p, category))
             else:
                 if category_value not in valid_values_set:
-                    message.append(("invalid category value", p, category))
-    return message
+                    invalid_points.append(("invalid category value", p, category))
+    return invalid_points
 
 
 def get_fields_with_null_values(datapoints):
-    message = []
+    invalid_points = []
     for p in datapoints:
         for attribute, value in p.items():
             if value is None:
-                message.append(("null value", p, attribute))
-    return message
+                invalid_points.append(("null value", p, attribute))
+    return invalid_points
 
 
 def validate_from_json(json_data):
@@ -42,25 +42,25 @@ def validate_from_json(json_data):
     categories = map_data["categories"]
     obligatory_fields = map_data["obligatory_fields"]
 
-    error_messages = []
+    invalid_points = []
 
-    error_messages += get_missing_obligatory_fields(datapoints, obligatory_fields)
-    error_messages += get_categories_with_invalid_values(datapoints, categories)
-    error_messages += get_fields_with_null_values(datapoints)
+    invalid_points += get_missing_obligatory_fields(datapoints, obligatory_fields)
+    invalid_points += get_categories_with_invalid_values(datapoints, categories)
+    invalid_points += get_fields_with_null_values(datapoints)
 
-    return error_messages
+    return invalid_points
 
 
 def validate_from_json_file(path_to_json_file):
     with open(path_to_json_file) as json_file:
         try:
-            json_data = json.load(json_file)
+            json_database = json.load(json_file)
         except json.JSONDecodeError as e:
             print("DATA ERROR: invalid json format", stderr)
             print(e)
             exit(-1)
 
-    return validate_from_json(json_data)
+    return validate_from_json(json_database)
 
 
 if __name__ == "__main__":
