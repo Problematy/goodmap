aa1ce78d38cc26e771a8afffb3129a39fdefbbee
Miłosz Nowaczyk <milosz.nowaczyk@protonmail.com>
2024-10-07 17:50:52 +0200

    feat: modify endpoints to handle accessibility table

--- goodmap/core.py
+++ goodmap/core.py
@@ -9,6 +9,28 @@ def does_fulfill_requirement(entry, requirements):
         matches.append(all(entry_value in entry[category] for entry_value in values))
     return all(matches)
 
+def sort_by_lat_lon(data, query_params):
+    try: 
+        if "lat" in query_params and "lon" in query_params:
+            lat = float(query_params["lat"][0])
+            lon = float(query_params["lon"][0])
+            data.sort(key=lambda x: (x['position'][0] - lat) ** 2 + (x['position'][1] - lon) ** 2)
+            return data
+        return data
+    except:
+        return data
+
+
+def limit(data, query_params, limit):
+    try: 
+        if "limit" in query_params:
+            limit = int(query_params["limit"][0])
+            data = data[:limit]
+            return data
+        return data
+    except:
+        return data
+        
 
 def get_queried_data(all_data, categories, query_params):
     requirements = []
@@ -16,4 +38,6 @@ def get_queried_data(all_data, categories, query_params):
         requirements.append((key, query_params.get(key)))
 
     filtered_data = [x for x in all_data if does_fulfill_requirement(x, requirements)]
-    return filtered_data
+    final_data = sort_by_lat_lon(filtered_data, query_params)
+    final_data = limit(final_data, query_params, 10)
+    return final_data
--- tests/e2e_tests/cypress.config.js
+++ tests/e2e_tests/cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
+const {
+  cypressBrowserPermissionsPlugin,
+} = require("cypress-browser-permissions");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://www.goodmap.localhost:5000/',
+    baseUrl: "http://www.localhost:5000/",
     setupNodeEvents(on, config) {
+      config = cypressBrowserPermissionsPlugin(on, config)
       return config;
-    }
-}
+    },
+  },
+  env: {
+    browserPermissions: {
+      geolocation: "allow",
+    },
+  },
 });
