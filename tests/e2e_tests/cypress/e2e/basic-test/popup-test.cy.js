c93f20266a8fb042a968354c1b3fb0bb677644b3
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-10-29 12:06:06 +0100

    Add popup test checking title and subtitle are displayed

---
+++ tests/e2e_tests/cypress/e2e/basic-test/popup-test.cy.js
@@ -0,0 +1,45 @@
+describe('CTA Tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.wait(3000);
+  })
+  const expectedPlace1 = {
+    title: "Grunwaldzki",
+    subtitle: "big bridge",
+  };
+  const expectedPlace2 = {
+    title: "Zwierzyniecka",
+    subtitle: "small bridge",
+  };
+
+  it('displays CTA button', () => {
+    const zoomInTimes = 1;
+    for (let i = 0; i < zoomInTimes; i++) {
+      cy.get('.leaflet-marker-icon').first().click();
+      cy.wait(500);
+    }
+
+    cy.get('.leaflet-marker-icon').first().then(($marker) => {
+      cy.wrap($marker).click();
+      cy.wait(500);
+
+      cy.get('.leaflet-popup-content')
+        .should('exist')
+        .within(() => {
+
+          cy.get('.point-title')
+            .should('exist')
+            .invoke('text')
+            .then((title) => {
+              if (title === expectedPlace1.title) {
+                cy.get('.point-subtitle')
+                  .should('have.text', expectedPlace1.subtitle)
+              } else if (title === expectedPlace2.title) {
+                cy.get('.point-subtitle')
+                  .should('have.text', expectedPlace2.subtitle)
+              }
+            });
+        });
+    });
+  });
+});
