9a8a156151c717d72a20fff4c9814bddda80945b
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-11-06 10:46:13 +0100

    some duplications extracted

--- tests/unit_tests/test_db.py
+++ tests/unit_tests/test_db.py
@@ -1,48 +1,33 @@
 from unittest import mock
-
 from platzky.db.google_json_db import GoogleJsonDb
 from platzky.db.json_db import Json
 from platzky.db.json_file_db import JsonFile
-
 from goodmap.data_models.location import LocationBase
 from goodmap.db import goodmap_db_extended_app
 
+import json
+
+expected_data = {"data": [{"position": [50, 50], "UUID": "1"}, {"position": [10, 10], "UUID": "2"}]}
+data_json = json.dumps({"map": expected_data})
+
+
+def initialize_and_assert_db(db, data):
+    goodmap_db_extended_app(db, LocationBase)
+    assert db.get_location("1") == LocationBase(position=(50, 50), UUID="1")
+    assert len(db.get_locations()) == 2
+    assert db.get_data() == data
 
 def test_goodmap_json_db_extended():
-    data = {"data": [{"position": [50, 50], "UUID": "1"}, {"position": [10, 10], "UUID": "2"}]}
-    db = Json(data)
-    goodmap_db_extended_app(db, LocationBase)
-    assert db.get_location("1") == LocationBase(position=(50, 50), UUID="1")  # pyright: ignore
-    assert len(db.get_locations()) == 2  # pyright: ignore
-    assert db.get_data() == data  # pyright: ignore
+    db = Json(expected_data)
+    initialize_and_assert_db(db, expected_data)
 
-
-@mock.patch(
-    "builtins.open",
-    mock.mock_open(
-        read_data='{"map":{"data":[{"position": [50, 50], "UUID": "1"},\
-         {"position": [10, 10], "UUID": "2"}]}}'
-    ),
-)
+@mock.patch("builtins.open", mock.mock_open(read_data=data_json))
 def test_goodmap_json_db_file_extended():
-    expected_data = {
-        "data": [{"position": [50, 50], "UUID": "1"}, {"position": [10, 10], "UUID": "2"}]
-    }
     db = JsonFile("/fake/path/file.json")
-    goodmap_db_extended_app(db, LocationBase)
-    assert db.get_location("1") == LocationBase(position=(50, 50), UUID="1")  # pyright: ignore
-    assert len(db.get_locations()) == 2  # pyright: ignore
-    assert db.get_data() == expected_data  # pyright: ignore
-
+    initialize_and_assert_db(db, expected_data)
 
 @mock.patch("platzky.db.google_json_db.Client")
 def test_goodmap_google_json_db_extended(mock_cli):
-    mock_cli.return_value.bucket.return_value.blob.return_value.download_as_text.return_value = '{\
-      "map":{"data":[{"position": [50, 50], "UUID": "1"}, {"position": [10, 10], "UUID": "2"}]}}'
+    mock_cli.return_value.bucket.return_value.blob.return_value.download_as_text.return_value = data_json
     db = GoogleJsonDb("bucket", "blob")
-    goodmap_db_extended_app(db, LocationBase)
-    assert db.get_location("1") == LocationBase(position=(50, 50), UUID="1")  # pyright: ignore
-    assert len(db.get_locations()) == 2  # pyright: ignore
-    assert db.get_data() == {  # pyright: ignore
-        "data": [{"position": [50, 50], "UUID": "1"}, {"position": [10, 10], "UUID": "2"}]
-    }
+    initialize_and_assert_db(db, expected_data)
