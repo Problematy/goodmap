d90aee2d51fce9336f629d4e9d2ef9b3e02f9fd0
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-11-06 10:49:36 +0100

    lint fixes

--- tests/unit_tests/test_db.py
+++ tests/unit_tests/test_db.py
@@ -1,11 +1,12 @@
+import json
 from unittest import mock
+
 from platzky.db.google_json_db import GoogleJsonDb
 from platzky.db.json_db import Json
 from platzky.db.json_file_db import JsonFile
+
 from goodmap.data_models.location import LocationBase
 from goodmap.db import goodmap_db_extended_app
-
-import json
 
 expected_data = {"data": [{"position": [50, 50], "UUID": "1"}, {"position": [10, 10], "UUID": "2"}]}
 data_json = json.dumps({"map": expected_data})
@@ -17,17 +18,22 @@ def initialize_and_assert_db(db, data):
     assert len(db.get_locations()) == 2
     assert db.get_data() == data
 
+
 def test_goodmap_json_db_extended():
     db = Json(expected_data)
     initialize_and_assert_db(db, expected_data)
+
 
 @mock.patch("builtins.open", mock.mock_open(read_data=data_json))
 def test_goodmap_json_db_file_extended():
     db = JsonFile("/fake/path/file.json")
     initialize_and_assert_db(db, expected_data)
 
+
 @mock.patch("platzky.db.google_json_db.Client")
 def test_goodmap_google_json_db_extended(mock_cli):
-    mock_cli.return_value.bucket.return_value.blob.return_value.download_as_text.return_value = data_json
+    mock_cli.return_value.bucket.return_value.blob.return_value.download_as_text.return_value = (
+        data_json
+    )
     db = GoogleJsonDb("bucket", "blob")
     initialize_and_assert_db(db, expected_data)
