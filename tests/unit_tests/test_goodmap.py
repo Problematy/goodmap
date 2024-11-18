2eeaf0ab28edc24760f9b8865f84337cd19bb3e1
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-11-18 01:37:41 +0100

    fixed test

--- tests/unit_tests/test_goodmap.py
+++ tests/unit_tests/test_goodmap.py
@@ -20,7 +20,7 @@ def test_create_app():
 
 def test_create_app_from_config():
     with patch("platzky.platzky.create_app_from_config", MagicMock()) as mock_platzky_app_creation:
-        with patch("goodmap.goodmap.goodmap_db_extended_app", MagicMock()) as mock_extend_db:
+        with patch("goodmap.goodmap.extend_db_with_goodmap_queries", MagicMock()) as mock_extend_db:
             goodmap.create_app_from_config(config)
             mock_platzky_app_creation.assert_called_once_with(config)
             mock_extend_db.assert_called_once()
