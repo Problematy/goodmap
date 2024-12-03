9e14af0f537fc4cae733f225308c309dc9cbaa77
ravenwing <krzysztof.kolodzinski@problematy.pl>
2024-12-03 16:30:29 +0100

    Fix: submit button is translated

--- frontend/src/components/MarkerPopup/ReportProblemForm.jsx
+++ frontend/src/components/MarkerPopup/ReportProblemForm.jsx
@@ -101,7 +101,7 @@ export const ReportProblemForm = ({ placeId }) => {
                     />
                 </Label>
             )}
-            {problemType !== `` && <SubmitButton type="submit" value="Submit" />}
+            {problemType !== `` && <SubmitButton type="submit" value={t('submitProblem')} />}
         </Form>
     );
 };
