6a5f0399dc1a42018cdbd7536b2880abff722df3
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-05 14:02:44 +0100

    Improve verifying Problem Form

--- tests/e2e_tests/cypress/e2e/basic-test/commons.js
+++ tests/e2e_tests/cypress/e2e/basic-test/commons.js
@@ -55,6 +55,7 @@ function verifyPopupContent(expectedContent) {
 
 export function verifyProblemForm() {
   cy.contains('report a problem').should('exist').click();
+  cy.intercept('POST', '/api/report-location').as('reportLocation');
 
   cy.get('form').should('exist').within(() => {
     cy.get('select').should('exist').within(() => {
@@ -72,8 +73,18 @@ export function verifyProblemForm() {
 
     cy.get('select').select('other');
     cy.get('input[name="problem"]').should('exist');
-
     cy.get('input[name="problem"]').type('Custom issue description');
     cy.get('input[type="submit"]').should('exist').click();
   });
+
+  cy.wait('@reportLocation').then((interception) => {
+    expect(interception.request.body).to.have.property('id');
+    expect(interception.request.body).to.have.property('description');
+    expect(interception.request.body.description).to.equal('Custom issue description');
+    expect(interception.response.statusCode).to.equal(200);
+    expect(interception.response.body.message).to.equal('Location reported');
+  });
+
+  cy.contains('p', 'Location reported').should('exist');
+  cy.get('form').should('not.exist');
 }
