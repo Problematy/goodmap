331eebeb9c4fa452cc16b653b4588fdeedf37f68
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-09 14:06:29 +0100

    Wait 1000

--- tests/e2e_tests/cypress/e2e/basic-test/recorded-test.cy.js
+++ tests/e2e_tests/cypress/e2e/basic-test/recorded-test.cy.js
@@ -8,7 +8,7 @@ describe("Recording 09/12/2024 at 11:45:39", () => {
   })
   it("tests Recording 09/12/2024 at 11:45:39", () => {
     cy.get('.leaflet-marker-icon > div').click();
-    cy.wait(4500)
+    cy.wait(1000)
     cy.get('[style="margin-left: -12px; margin-top: -41px; width: 25px; height: 41px; transform: translate3d(451px, 333px, 0px); z-index: 333; opacity: 1;"]').click();
     cy.get('.leaflet-popup-content').should('exist')
       .within(() => {
