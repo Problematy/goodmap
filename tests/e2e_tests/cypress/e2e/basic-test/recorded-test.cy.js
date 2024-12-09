028437738027d8c5ae8d0ecefea3fd357aee8e11
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-09 13:56:59 +0100

    Increase waiting time, maybe this helps on pipeline

--- tests/e2e_tests/cypress/e2e/basic-test/recorded-test.cy.js
+++ tests/e2e_tests/cypress/e2e/basic-test/recorded-test.cy.js
@@ -2,11 +2,13 @@ import { verifyPopupContent, expectedPlaces } from "./commons.js"
 
 
 describe("Recording 09/12/2024 at 11:45:39", () => {
-  it("tests Recording 09/12/2024 at 11:45:39", () => {
+  beforeEach(() => {
     cy.visit('/');
-    cy.wait(500)
+    cy.wait(500);
+  })
+  it("tests Recording 09/12/2024 at 11:45:39", () => {
     cy.get('.leaflet-marker-icon > div').click();
-    cy.wait(500)
+    cy.wait(4500)
     cy.get('[style="margin-left: -12px; margin-top: -41px; width: 25px; height: 41px; transform: translate3d(451px, 333px, 0px); z-index: 333; opacity: 1;"]').click();
     cy.get('.leaflet-popup-content').should('exist')
       .within(() => {
