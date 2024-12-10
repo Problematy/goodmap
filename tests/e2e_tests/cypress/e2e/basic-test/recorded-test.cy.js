2e510fc98283c12297a52d35ef792548512d2a86
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-10 14:53:07 +0100

    Add missing semicolons

--- tests/e2e_tests/cypress/e2e/basic-test/recorded-test.cy.js
+++ tests/e2e_tests/cypress/e2e/basic-test/recorded-test.cy.js
@@ -11,17 +11,17 @@ describe("Recording 09/12/2024 at 11:45:39", () => {
       cy.stub(win, 'open').as('openStub');
     });
     cy.get('.leaflet-marker-icon > div').click();
-    cy.wait(1000)
+    cy.wait(1000);
     cy.get('[style="margin-left: -12px; margin-top: -41px; width: 25px; height: 41px; transform: translate3d(451px, 333px, 0px); z-index: 333; opacity: 1;"]').click();
-    cy.wait(1000)
+    cy.wait(1000);
     cy.get('.leaflet-popup-content').should('exist')
       .within(() => {
         verifyPopupContent(expectedPlaces[1]);
       });
 
-    cy.wait(1000)
+    cy.wait(1000);
     cy.get('.leaflet-popup-close-button > span').click();
-    cy.wait(1000)
+    cy.wait(1000);
 
     /* ==== Generated with Cypress Studio ==== */
     cy.get('[style="margin-left: -12px; margin-top: -41px; width: 25px; height: 41px; transform: translate3d(381px, 223px, 0px); z-index: 223; opacity: 1;"]').click();
