c06a8a378adc9fc7b5814ba1a9ef33a0f2b04b73
Dawid Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-12 16:09:32 +0100

    Refactored e2e tests

--- e2e-tests/tests/e2e_tests/cypress/e2e/basic-test/popup-test.cy.js
+++ e2e-tests/tests/e2e_tests/cypress/e2e/basic-test/popup-test.cy.js
@@ -1,33 +1,27 @@
-import { verifyArbitraryPopupContent, expectedPlaces } from "./commons.js"
+import { verifyPopupContent, expectedPlaceZwierzyniecka, getRightmostMarker } from "./commons.js"
 
-describe('Popup Tests', () => {
-  beforeEach(() => {
+describe("Popup Tests on desktop", () => {
+
+  it("displays popup title, subtitle, categories and CTA", () => {
     cy.visit('/');
-    cy.wait(500);
-  })
 
-  it('displays title and subtitle in the popup', () => {
     cy.window().then((win) => {
       cy.stub(win, 'open').as('openStub');
     });
 
-    cy.get('.leaflet-marker-icon').first().click();
-    cy.wait(500);
+    cy.get('.leaflet-marker-icon').click();
+    cy.get('.leaflet-marker-icon').should('have.length', 2);
 
-    cy.get('.leaflet-marker-icon').each(($marker) => {
-      cy.wrap($marker).click();
-      cy.wait(500);
-      cy.get('.leaflet-popup-content').should('exist')
-        .within(() => {
-          verifyArbitraryPopupContent(expectedPlaces);
-
-          // TODO BUG: when problem form is opened on desktop, the close button may be hidden
-          // Fix this and add checking problem form in this test
-        });
-      cy.get('.leaflet-popup-close-button').should('exist').then(($button) => {
-        cy.wrap($button).click();
-        cy.wait(500);
-      });
+    cy.get('.leaflet-marker-icon').then((markers) => {
+      const rightmostMarker = getRightmostMarker(markers);
+      cy.wrap(rightmostMarker).click();
     });
+
+    cy.get('.leaflet-popup-content').should('exist').within(() => {
+      verifyPopupContent(expectedPlaceZwierzyniecka);
+      // TODO BUG: when problem form is opened on desktop, the close button may be hidden
+      // Fix this and add checking problem form in this test
+    });
+    cy.get('.leaflet-popup-close-button').should('exist').click();
   });
 });
