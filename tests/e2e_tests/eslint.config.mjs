e79315e4580400bc4d81dae9bf20a583ec05f78f
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-18 13:53:33 +0100

    Style

--- tests/e2e_tests/eslint.config.mjs
+++ tests/e2e_tests/eslint.config.mjs
@@ -6,19 +6,21 @@ export default [
   eslintConfigPrettier,
   {
     rules: {
-      'cypress/unsafe-to-chain-command': 'error',
       'cypress/no-unnecessary-waiting': 'error',
-      'array-bracket-spacing': 'off',
-      'space-in-parens': 'off',
-      'no-unused-vars': 'warn',
-      'no-console': 'warn',
-      'func-names': 'error',
-      'no-process-exit': 'error',
-      'object-shorthand': 'error',
+      'cypress/unsafe-to-chain-command': 'error',
+
       'class-methods-use-this': 'error',
+      'func-names': 'error',
+      'no-console': 'warn',
+      'no-process-exit': 'error',
+      'no-unused-vars': 'warn',
+      'object-shorthand': 'error',
+
+      'array-bracket-spacing': 'off',
       'comma-dangle': 'off',
       'es-x/no-rest-spread-properties': 'off',
-      indent: ['error', 2],
+      'indent': ['error', 2],
+      'space-in-parens': 'off',
     }
   }
 ];
