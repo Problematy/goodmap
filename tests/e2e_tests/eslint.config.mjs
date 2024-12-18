c24a387aa01bef1c5949b6fb4f26174e86bb7824
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-18 12:32:09 +0100

    Add pluginCypress.configs.recommended

--- tests/e2e_tests/eslint.config.mjs
+++ tests/e2e_tests/eslint.config.mjs
@@ -1,10 +1,8 @@
 import pluginCypress from 'eslint-plugin-cypress/flat';
 
 export default [
+  pluginCypress.configs.recommended,
   {
-    plugins: {
-      cypress: pluginCypress
-    },
     rules: {
       'cypress/unsafe-to-chain-command': 'error',
       'cypress/no-unnecessary-waiting': 'error',
