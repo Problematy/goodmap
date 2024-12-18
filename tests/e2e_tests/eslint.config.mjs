4c39fbb1101514412e5de8c89e07bc4fb815e47c
Dawid Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-18 14:50:55 +0100

    Add eslint and prettier for checking cypress tests (#200)

---
+++ tests/e2e_tests/eslint.config.mjs
@@ -0,0 +1,29 @@
+import pluginCypress from 'eslint-plugin-cypress/flat';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  pluginCypress.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      'cypress/no-unnecessary-waiting': 'error',
+      'cypress/unsafe-to-chain-command': 'error',
+      'cypress/assertion-before-screenshot': 'error',
+      'cypress/require-data-selectors': 'warn',
+      'cypress/no-force': 'warn',
+
+      'class-methods-use-this': 'error',
+      'func-names': 'error',
+      'no-console': 'error',
+      'no-process-exit': 'error',
+      'no-unused-vars': 'warn',
+      'object-shorthand': 'error',
+
+      'array-bracket-spacing': 'off',
+      'comma-dangle': 'off',
+      'es-x/no-rest-spread-properties': 'off',
+      'indent': ['error', 4],
+      'space-in-parens': 'off',
+    }
+  }
+];
