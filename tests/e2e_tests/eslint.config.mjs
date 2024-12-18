f059c1a1ae05f7e3ca5b1a6a7faf16d93f334ea3
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-18 12:10:32 +0100

    More eslint rules

--- tests/e2e_tests/eslint.config.mjs
+++ tests/e2e_tests/eslint.config.mjs
@@ -1,4 +1,5 @@
-import pluginCypress from 'eslint-plugin-cypress/flat'
+import pluginCypress from 'eslint-plugin-cypress/flat';
+
 export default [
   {
     plugins: {
@@ -6,7 +7,18 @@ export default [
     },
     rules: {
       'cypress/unsafe-to-chain-command': 'error',
-      'cypress/no-unnecessary-waiting': 'error'
+      'cypress/no-unnecessary-waiting': 'error',
+      'array-bracket-spacing': 'off',
+      'space-in-parens': 'off',
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'func-names': 'error',
+      'no-process-exit': 'error',
+      'object-shorthand': 'error',
+      'class-methods-use-this': 'error',
+      'comma-dangle': 'off',
+      'es-x/no-rest-spread-properties': 'off',
+      indent: ['error', 2],
     }
   }
-]
+];
