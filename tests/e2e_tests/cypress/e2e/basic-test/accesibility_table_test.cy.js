aef4c9ec65021150fd2fd38ec5742e8c798d3be4
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2025-01-11 23:11:04 +0100

    fixed linting

--- tests/e2e_tests/cypress/e2e/basic-test/accesibility_table_test.cy.js
+++ tests/e2e_tests/cypress/e2e/basic-test/accesibility_table_test.cy.js
@@ -1,35 +1,35 @@
 function rowElements() {
-  return cy.get("tr");
+    return cy.get('tr');
 }
 
 function fakeLocation(latitude, longitude) {
-  return {
-    onBeforeLoad(win) {
-      cy.stub(win.navigator.geolocation, "getCurrentPosition", (cb, err) => {
-        if (latitude && longitude) {
-          return cb({ coords: { latitude, longitude } });
-        }
-        throw err({ code: 1 });
-      });
-    },
-  };
+    return {
+        onBeforeLoad(win) {
+            cy.stub(win.navigator.geolocation, 'getCurrentPosition', (cb, err) => {
+                if (latitude && longitude) {
+                    return cb({ coords: { latitude, longitude } });
+                }
+                throw err({ code: 1 });
+            });
+        },
+    };
 }
 
-describe("Accessibility table test", () => {
-  beforeEach(() => {
-    const latitude = 51.10655;
-    const longitude = 17.0555;
-    cy.visit("/", fakeLocation(latitude, longitude));
-    cy.get('button[id="listViewButton"]').click();
-  });
+describe('Accessibility table test', () => {
+    beforeEach(() => {
+        const latitude = 51.10655;
+        const longitude = 17.0555;
+        cy.visit('/', fakeLocation(latitude, longitude));
+        cy.get('button[id="listViewButton"]').click();
+    });
 
-  it("should properly display places", () => {
-    rowElements()
-      // Header + 2 rows
-      .should("have.length", 3);
-  });
+    it('should properly display places', () => {
+        rowElements()
+            // Header + 2 rows
+            .should('have.length', 3);
+    });
 
-  it("should 'Zwierzyniecka' be first row", () => {
-    rowElements().eq(1).find("td").should("contain", "Zwierzyniecka");
-  });
+    it("should 'Zwierzyniecka' be first row", () => {
+        rowElements().eq(1).find('td').should('contain', 'Zwierzyniecka');
+    });
 });
