98fc40e457b4d22bc8c808adcd77220c208986f3
Miłosz Nowaczyk <milosz.nowaczyk@protonmail.com>
2025-01-17 15:35:58 +0100

    tests: add more unti tests

--- tests/unit_tests/test_core.py
+++ tests/unit_tests/test_core.py
@@ -1,4 +1,4 @@
-from goodmap.core import does_fulfill_requirement, get_queried_data, limit
+from goodmap.core import does_fulfill_requirement, get_queried_data, limit, sort_by_distance
 
 test_data = [
     {
@@ -114,3 +114,64 @@ def test_that_limit_does_not_change_data_if_limit_not_specified():
     ]
     limit_returned_data = limit(test_data, {})
     assert limit_returned_data == expected_data
+
+
+def test_that_sort_by_distance_works_as_intended():
+    expected_data = [
+        {
+            "name": "PCK",
+            "position": [51.1, 17.05],
+            "types": ["clothes"],
+            "gender": ["male"],
+        },
+        {
+            "name": "LASSO",
+            "position": [51.113, 17.06],
+            "types": ["shoes"],
+            "gender": ["male", "female"],
+        },
+    ]
+
+    query_params = {"lat": ["51.1"], "lon": ["17.05"]}
+    sorted_data = sort_by_distance(test_data, query_params)
+    assert sorted_data == expected_data
+
+
+def test_that_sort_by_distance_returns_data_when_query_params_are_corrupted():
+    expected_data = [
+        {
+            "name": "PCK",
+            "position": [51.1, 17.05],
+            "types": ["clothes"],
+            "gender": ["male"],
+        },
+        {
+            "name": "LASSO",
+            "position": [51.113, 17.06],
+            "types": ["shoes"],
+            "gender": ["male", "female"],
+        },
+    ]
+    query_params = {"lat": ["51c.o1ruppted"], "lon": ["17c.05ruptted"]}
+    sorted_data = sort_by_distance(test_data, query_params)
+    assert sorted_data == expected_data
+
+
+def test_that_limit_returns_data_when_query_params_are_corrupted():
+    expected_data = [
+        {
+            "name": "PCK",
+            "position": [51.1, 17.05],
+            "types": ["clothes"],
+            "gender": ["male"],
+        },
+        {
+            "name": "LASSO",
+            "position": [51.113, 17.06],
+            "types": ["shoes"],
+            "gender": ["male", "female"],
+        },
+    ]
+    query_params = {"limit": ["1c0rupte0d"]}
+    limit_data = limit(test_data, query_params)
+    assert limit_data == expected_data
